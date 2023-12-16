61cdc51fbd872ce31e73364ef25f9a69fc7ab5f4
leticiacareli <leticia_careli@hotmail.com>
2023-12-16 18:42:43 -0300

    bug: keyType uuid

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,6 +10,9 @@ class Course extends Model
 {
     use HasFactory, UuidTrait;
 
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+
     protected $fillable = [
         'name',
         'description',
